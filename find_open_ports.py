92f68c2a0605fecbea5760d860d90ce62fe2b6d8
opatuo <rmortam@gmail.com>
2025-03-14 15:18:13 -0400

    Update find_open_ports.py
    * Add get_variables_passed_to

--- find_open_ports.py
+++ find_open_ports.py
@@ -12,6 +12,14 @@ def get_calls_to(binary_view, function_name: str):
     return binary_view.get_code_refs(symbol.address)
 
 
+def get_variables_passed_to(function, reference):
+    function_mlil = function.mlil
+    call_instruction = function_mlil[
+        function_mlil.get_instruction_start(reference.address)
+    ].ssa_form
+    return call_instruction.params
+
+
 parser = argparse.ArgumentParser(
     prog="Find Open Ports", description="Search a binary for open ports"
 )
@@ -20,10 +28,13 @@ parser.add_argument("--verbose", action="store_true")
 args = parser.parse_args()
 
 if not args.verbose:
-    disable_default_log()
+    binaryninja.disable_default_log()
 
 with binaryninja.load(args.filename) as binary_view:
     for reference in get_calls_to(binary_view, "bind"):
         callers = binary_view.get_functions_containing(reference.address)
         for caller in callers:
-            pass
+            socket_fd, sockaddr, sockaddr_size = get_variables_passed_to(
+                caller, reference
+            )
+            print(sockaddr)
