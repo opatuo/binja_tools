c2b7e1c505be115b4b59e7e8752ffbe029cdd137
opatuo <rmortam@gmail.com>
2025-03-20 20:59:02 -0400

    Remove uneeded comments

--- binjalib/varslice.py
+++ binjalib/varslice.py
@@ -19,13 +19,11 @@ def backward(
         string, variable_read, variable_type = operand
         if string == "src":
             # could be the address of another variable or function call
-            # TODO: need to find the SSAVariable that is the source
             if (
                 variable_read.ssa_form.operation
                 == binaryninja.enums.MediumLevelILOperation.MLIL_ADDRESS_OF
             ):
                 return variable_read.src
-            # if variable_read.is_parameter_variable:
             return variable
 
     # No more src variables, parse expression for output
