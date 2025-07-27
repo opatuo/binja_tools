110b9590ad0be07a637489324d7a33e2649cf98c
Rhea <rmortam@gmail.com>
2025-07-27 14:32:49 -0400

    Formatting updates and flesh out instruction text

--- plugins/bn-mips16/architecture.py
+++ plugins/bn-mips16/architecture.py
@@ -4,8 +4,9 @@ from binaryninja import Endianness
 
 from .disassembler import MIPS16Disassembler
 
+
 class MIPS16Architecture(Architecture):
-    name = 'mips16'
+    name = "mips16"
     max_instr_length = 4
     address_size = 4
     default_int_size = 4
@@ -18,39 +19,39 @@ class MIPS16Architecture(Architecture):
     # we are using the ABI names here because those are the register names returned by capstone.
     # https://refspecs.linuxfoundation.org/elf/mipsabi.pdf
     regs = {
-              'zero': RegisterInfo('zero', address_size),
-              'at': RegisterInfo('at', address_size),
-              'v0': RegisterInfo('v0', address_size),
-              'v1': RegisterInfo('v1', address_size),
-              'a0': RegisterInfo('a0', address_size),
-              'a1': RegisterInfo('a1', address_size),
-              'a2': RegisterInfo('a2', address_size),
-              'a3': RegisterInfo('a3', address_size),
-              't0': RegisterInfo('t0', address_size),
-              't1': RegisterInfo('t1', address_size),
-              't2': RegisterInfo('t2', address_size),
-              't3': RegisterInfo('t3', address_size),
-              't4': RegisterInfo('t4', address_size),
-              't5': RegisterInfo('t5', address_size),
-              't6': RegisterInfo('t6', address_size),
-              't7': RegisterInfo('t7', address_size),
-              's0': RegisterInfo('s0', address_size),
-              's1': RegisterInfo('s1', address_size),
-              's2': RegisterInfo('s2', address_size),
-              's3': RegisterInfo('s3', address_size),
-              's4': RegisterInfo('s4', address_size),
-              's5': RegisterInfo('s5', address_size),
-              's6': RegisterInfo('s6', address_size),
-              's7': RegisterInfo('s7', address_size),
-              't8': RegisterInfo('t8', address_size),
-              't9': RegisterInfo('t9', address_size),
-              'kt0': RegisterInfo('kt0', address_size),
-              'kt1': RegisterInfo('kt1', address_size),
-              'gp': RegisterInfo('gp', address_size),
-              'sp': RegisterInfo('sp', address_size),
-              'fp': RegisterInfo('fp', address_size), # also s8
-              'ra': RegisterInfo('ra', address_size),
-           }
+        "zero": RegisterInfo("zero", address_size),
+        "at": RegisterInfo("at", address_size),
+        "v0": RegisterInfo("v0", address_size),
+        "v1": RegisterInfo("v1", address_size),
+        "a0": RegisterInfo("a0", address_size),
+        "a1": RegisterInfo("a1", address_size),
+        "a2": RegisterInfo("a2", address_size),
+        "a3": RegisterInfo("a3", address_size),
+        "t0": RegisterInfo("t0", address_size),
+        "t1": RegisterInfo("t1", address_size),
+        "t2": RegisterInfo("t2", address_size),
+        "t3": RegisterInfo("t3", address_size),
+        "t4": RegisterInfo("t4", address_size),
+        "t5": RegisterInfo("t5", address_size),
+        "t6": RegisterInfo("t6", address_size),
+        "t7": RegisterInfo("t7", address_size),
+        "s0": RegisterInfo("s0", address_size),
+        "s1": RegisterInfo("s1", address_size),
+        "s2": RegisterInfo("s2", address_size),
+        "s3": RegisterInfo("s3", address_size),
+        "s4": RegisterInfo("s4", address_size),
+        "s5": RegisterInfo("s5", address_size),
+        "s6": RegisterInfo("s6", address_size),
+        "s7": RegisterInfo("s7", address_size),
+        "t8": RegisterInfo("t8", address_size),
+        "t9": RegisterInfo("t9", address_size),
+        "kt0": RegisterInfo("kt0", address_size),
+        "kt1": RegisterInfo("kt1", address_size),
+        "gp": RegisterInfo("gp", address_size),
+        "sp": RegisterInfo("sp", address_size),
+        "fp": RegisterInfo("fp", address_size),  # also s8
+        "ra": RegisterInfo("ra", address_size),
+    }
 
     stack_pointer = "sp"
 
--- plugins/bn-mips16/disassembler.py
+++ plugins/bn-mips16/disassembler.py
@@ -1,27 +1,29 @@
 from capstone import *
 from capstone.mips import *
-from binaryninja import InstructionInfo
+from binaryninja import InstructionInfo, InstructionTextToken, InstructionTextTokenType
+
 
 class MIPS16Disassembler:
     def __init__(self):
-        self.disassembler = Cs(CS_ARCH_MIPS,  CS_MODE_MIPS32 + CS_MODE_BIG_ENDIAN)
+        self.disassembler = Cs(CS_ARCH_MIPS, CS_MODE_MIPS32 + CS_MODE_BIG_ENDIAN)
         self.disassembler.detail = True
 
         self.micro_mode = False
         self.address_length = 4
 
     def decode(self, data, address):
-        raw_instruction = _get_raw_instruction(self, data, address)
+        raw_instruction = self._get_raw_instruction(data, address)
         if raw_instruction is None:
             return None
-        return _decode_impl(self, instruction)
+        return self._decode_impl(instruction)
 
     def text(self, data, address):
-        raw_instruction = _get_raw_instruction(self, data, address)
+        raw_instruction = self._get_raw_instruction(data, address)
         if raw_instruction is None:
             return None
+        return self._text_impl(raw_instruction)
 
-    def address_length(self):
+    def _address_length(self):
         return self.address_length
 
     def _toggle_micro_mode_on(self):
@@ -44,5 +46,55 @@ class MIPS16Disassembler:
 
     def _decode_impl(self, raw_instruction):
         result = InstructionInfo()
-        result.length = address_length(self)
+        result.length = self._address_length()
         return result
+
+    def _text_impl(self, raw_instruction):
+        tokens = []
+        tokens.append(
+            InstructionTextToken(
+                InstructionTextTokenType.InstructionToken, raw_instruction.mnemonic
+            )
+        )
+        for i in raw_instruction.operands:
+            if i.type == MIPS_OP_REG:
+                tokens.append(
+                    InstructionTextToken(
+                        InstructionTextTokenType.RegisterToken,
+                        raw_instruction.reg_name(i.value.reg),
+                    )
+                )
+            if i.type == MIPS_OP_IMM:
+                tokens.append(
+                    InstructionTextToken(
+                        InstructionTextTokenType.IntegerToken,
+                        hex(i.value.imm),
+                        value=i.value.imm,
+                    )
+                )
+            if i.type == MIPS_OP_MEM:
+                tokens.append(
+                    InstructionTextToken(
+                        InstructionTextTokenType.BeginMemoryOperandToken, "("
+                    )
+                )
+                tokens.append(
+                    InstructionTextToken(
+                        InstructionTextTokenType.RegisterToken,
+                        raw_instruction.reg_name(i.value.mem.base),
+                    )
+                )
+                tokens.append(
+                    InstructionTextToken(
+                        InstructionTextTokenType.IntegerToken,
+                        hex(i.value.mem.disp),
+                        value=i.value.mem.disp,
+                    )
+                )
+                tokens.append(
+                    InstructionTextToken(
+                        InstructionTextTokenType.EndMemoryOperandToken, ")"
+                    )
+                )
+
+        return tokens, self._address_length()
